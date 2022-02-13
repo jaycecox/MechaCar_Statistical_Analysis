d1ffe99bf9be1abc01d4c920b76eaeeebd1347c5
jaycecox <92542382+jaycecox@users.noreply.github.com>
2022-02-13 15:40:10 -0500

    '

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,38 @@ summary(Mecha_lm)
 
 # view R-squared
 summary(Mecha_lm)$r.squared
+
+########################################################
+
+# import csv file
+Suspension_Coil <- read.csv(file='./Data/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# summary table 
+
+total_summary <- Suspension_Coil %>%
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Std_Dev=sd(PSI))
+total_summary
+
+
+# by lots 
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>%
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Std_Dev=sd(PSI))
+lot_summary
+
+######################################################
+
+# Hypothesis is that the mu = 1500
+# Alt Hypothesis is that mu is not = 1500
+
+t.test(Suspension_Coil$PSI, mu=1500)
+
+#Lot 1
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+
+#Lot 2
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+
+#Lot 3
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
+
+
